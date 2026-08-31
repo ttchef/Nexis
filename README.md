4e4a0476b215396a3e5788d0872386109e2fc166
ttchef <ruro0.edit@gmail.com>
2026-08-31 11:36:36 +0200

    add raylib and clay dependencies

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Nexis
+
+# Dependencies
+  - [Clay](https://github.com/nicbarker/clay.git)
+  - [Raylib](https://github.com/raysan5/raylib.git)
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 
-#include <iostream>
+#define CLAY_IMPLEMENTATION
+#include <clay.h>
+#include <renderers/raylib/clay_renderer_raylib.c>
 
 int main() {
-	std::cout << "Hello World\n";
+	SetTraceLogLevel(LOG_WARNING);
+	Clay_Raylib_Initialize(800, 600, "Title", FLAG_MSAA_4X_HINT | FLAG_WINDOW_RESIZABLE);
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawCircle(10, 10, 20, WHITE);
+		EndDrawing();
+	}
+	Clay_Raylib_Close();
 	return 0;
 }
